88dec07e9491b8505b5be2b004057f858263e87f
Zhasik-Kaukin <you@example.com>
2023-11-17 01:07:25 +0600

    fourth commit

--- flaskapp.py
+++ flaskapp.py
@@ -1,11 +1,12 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
-
+from flask_migrate import Migrate
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog6.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog8.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 app.secret_key = 'my_secret_key'
 
 
@@ -16,7 +17,7 @@ class User(db.Model):
     username = db.Column(db.String, nullable=False)
     password = db.Column(db.String, nullable=False)
     articles = db.relationship('Article', back_populates='user')
-    friends = db.relationship('Friendship', foreign_keys=[Friendship.user_id], backref='user', lazy='dynamic')
+    friends = db.relationship('Friendship', foreign_keys='Friendship.user_id', backref='user', lazy='dynamic')
 
     def __repr__(self):
         return f"<User % {self.id}>"
@@ -45,15 +46,14 @@ class Message(db.Model):
     def __repr__(self):
         return f"<Message % {self.id}>"
 
-
-class Friendship(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-    friend_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-    status = db.Column(db.String(20), default='pending')
-    date_added = db.Column(db.DateTime, default=datetime.utcnow)
-    user = db.relationship('User', foreign_keys=[user_id])
-    friend = db.relationship('User', foreign_keys=[friend_id])
+    class Friendship(db.Model):
+        id = db.Column(db.Integer, primary_key=True)
+        user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+        friend_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+        status = db.Column(db.String(20), default='pending')
+        date_added = db.Column(db.DateTime, default=datetime.utcnow)
+        user_rel = db.relationship('User', foreign_keys=[user_id], overlaps='friends,user')
+        friend_rel = db.relationship('User', foreign_keys=[friend_id], overlaps='friends,user')
 
     def __repr__(self):
         return f"<Friendship % {self.id}>"
